1559eb81191623ed540a23a9821a487f5dcd11a0
Math92 <mathiassan92@gmail.com>
2025-02-01 21:19:07 -0300

    Modifying AiB character images

--- src/data/charactersData.js
+++ src/data/charactersData.js
@@ -1,11 +1,19 @@
 // charactersData.js
+import arisuImage from '/src/assets/public/images/arisu.png';
+import usagiImage from '/src/assets/public/images/usagi.png';
+import chishiyaImage from '/src/assets/public/images/chishiya.png';
+import kuinaImage from '/src/assets/public/images/kuina.png';
+import annImage from '/src/assets/public/images/ann.png';
+import aguniImage from '/src/assets/public/images/aguni.png';
+import tattaImage from '/src/assets/public/images/tatta.png';
+
 export const characters = [
   {
     id: 1,
     name: "Arisu",
     description: "Experto en lógica y estrategia",
     specialAbility: "Bonus en juegos de ♠",
-    avatar: '/src/assets/public/images/arisu.PNG',
+    avatar: arisuImage, // Usa la variable importada
     image: 'https://via.placeholder.com/150/92c952'
   },
   {
@@ -13,7 +21,7 @@ export const characters = [
     name: "Usagi", 
     description: "Atleta profesional",
     specialAbility: "Bonus en juegos de ♥",
-    avatar: '/src/assets/public/images/usagi.PNG',
+    avatar: usagiImage,
     image: 'https://via.placeholder.com/150/771796'
   },
   {
@@ -21,7 +29,7 @@ export const characters = [
     name: "Chishiya",
     description: "Maestro de la manipulación",
     specialAbility: "Bonus en juegos de ♦",
-    avatar: '/src/assets/public/images/chishiya.PNG',
+    avatar: chishiyaImage,
     image: 'https://via.placeholder.com/150/24f355'
   },
   {
@@ -29,7 +37,7 @@ export const characters = [
     name: "Kuina",
     description: "Experta en combate",
     specialAbility: "Bonus en juegos físicos",
-    avatar: '/src/assets/public/images/kuina.PNG',
+    avatar: kuinaImage,
     image: 'https://via.placeholder.com/150/d32776'
   },
   {
@@ -37,7 +45,7 @@ export const characters = [
     name: "Ann",
     description: "Médica inteligente",
     specialAbility: "Bonus en juegos de estrategia",
-    avatar: '/src/assets/public/images/ann.PNG',
+    avatar: annImage,
     image: 'https://via.placeholder.com/150/f66b97'
   },
   {
@@ -45,7 +53,7 @@ export const characters = [
     name: "Aguni",
     description: "Lider de grupo",
     specialAbility: "Bonus en juegos de ♣",
-    avatar: '/src/assets/public/images/aguni.PNG',
+    avatar: aguniImage,
     image: 'https://via.placeholder.com/150/56a8c2'
   },
   {
@@ -53,7 +61,7 @@ export const characters = [
     name: "Tatta",
     description: "Colaborador Innato",
     specialAbility: "Bonus en juegos psicológicos",
-    avatar: '/src/assets/public/images/tatta.PNG',
+    avatar: tattaImage,
     image: 'https://via.placeholder.com/150/b0f7cc'
   }
 ];
